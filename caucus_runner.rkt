3e59ae7cc8f6c3ba352dae153971646a0e0f2d44
Mitch Gamburg <mitch.gamburg@gmail.com>
2020-11-18 15:11:37 -0500

    finished entirely with abstracted syndicate runner

--- caucus_runner.rkt
+++ caucus_runner.rkt
@@ -4,7 +4,12 @@
 
 (require json)
 (require "caucus.rkt")
+(require "test_runner.rkt")
+
 (require/activate syndicate/drivers/timestate)
+
+(define INPUT-FILE "example_test.json")
+(define OUTPUT-FILE "syndicate_output.json")
 
 (define (create-candidate jscand)
   (spawn-candidate (hash-ref jscand 'name)
@@ -12,34 +17,17 @@
                    (hash-ref jscand 'threshold)))
 
 (define (create-voter jsvoter region-name)
-  (spawn-voter (hash-ref voter 'name)
+  (spawn-voter (hash-ref jsvoter 'name)
                region-name
-               (stupid-sort (hash-ref (hash-ref voter 'voting_method) 'candidate))))
+               (stupid-sort (hash-ref (hash-ref jsvoter 'voting_method)
+                                      'candidate))))
 
-(define (create-manager 
+(define (create-manager regions)
+  (spawn-manager regions))
 
-
-  (spawn-manager (map (λ (region) (hash-ref region 'name)) (hash-ref test 'regions)))
-
-;; candidate -> void
-(define (initialize-candidate candidate)
-  (spawn-candidate (hash-ref candidate 'name)
-                   (hash-ref candidate 'tax_rate)
-                   (hash-ref candidate 'threshold)))
-
-;; region -> void
-(define (initialize-region region)
-  (define region-name (hash-ref region 'name))
-  (for ([voter (in-list (hash-ref region 'voters))])
-    (initialize-voter voter region-name)))
-
-;; voter string -> void
-(define (initialize-voter voter region-name)
-  (spawn-voter (hash-ref voter 'name)
-               region-name
-               (stupid-sort (hash-ref (hash-ref voter 'voting_method) 'candidate))))
-
-(define test-input "example_test.json")
-(define test (read-json (open-input-file test-input)))
-
-(initialize-test test "syndicate_output.json")
+(initialize-test INPUT-FILE
+                 OUTPUT-FILE
+                 create-candidate
+                 create-voter
+                 spawn-manager
+                 spawn-test-output-collector)
--- test_runner.rkt
+++ test_runner.rkt
@@ -1,18 +1,22 @@
 #lang racket
 
 (require json)
+(provide initialize-test)
 
 ;; do we need a test-output-collector? don't think so
 ;; need a create-manager
 ;; need a create-candidate
 ;; need a create-voter
 
-;; test -> void
-(define (initialize-test test
-                         output-file
-                         create-manager
-                         create-candidate
-                         create-voter)
+;; String String [JSCand -> void] [JSVoter Region -> void] 
+(define (initialize-test input-file       ;; String
+                         output-file      ;; String
+                         create-candidate ;; [JSCand -> void]
+                         create-voter     ;; [JSVoter Region -> void]
+                         create-manager   ;; [[List-of Region]
+                         create-test-collector)  ;; String -> void
+  (define test (read-json (open-input-file input-file)))
+
   (for ([candidate (in-list (hash-ref test 'candidates))])
     (create-candidate candidate))
 
@@ -20,16 +24,10 @@
     (initialize-region region create-voter))
 
   (create-manager (map (λ (region) (hash-ref region 'name)) (hash-ref test 'regions)))
+  (create-test-collector output-file))
 
+;; Region [JSRegion [JSVoter -> void]] -> void
 (define (initialize-region region create-voter)
   (define region-name (hash-ref region 'name))
   (for ([voter (in-list (hash-ref region 'voters))])
     (create-voter voter region-name)))
-
-
-
-;; candidate -> void
-(define (initialize-candidate candidate)
-  (spawn-candidate (hash-ref candidate 'name)
-                   (hash-ref candidate 'tax_rate)
-                   (hash-ref candidate 'threshold)))
